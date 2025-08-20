a2052fbba9958885dd9d0f0e435b85644fe333d1
Pablo Duran <duranpablo.alejandro@gmail.com>
2025-08-20 19:59:04 -0300

    nueva musica

--- PROTOTIPO5/secuenciaAbajo.gd
+++ PROTOTIPO5/secuenciaAbajo.gd
@@ -5,36 +5,57 @@ var data = [
 
 
 
-	{"tiempo": 4.0, "tecla": "space"},
+	{"tiempo": 3.969, "tecla": "space"},
 
-	{"tiempo": 6.0, "tecla": "space"},
+	{"tiempo": 6.073, "tecla": "space"},
 
-	{"tiempo": 8.0, "tecla": "space"},
-	{"tiempo": 9.0, "tecla": "space"},
+	{"tiempo": 7.945, "tecla": "space"},
 
-	{"tiempo": 11.0, "tecla": "space"},
-	{"tiempo": 12.0, "tecla": "space"},
-	{"tiempo": 13.0, "tecla": "space"},
-	{"tiempo": 14.0, "tecla": "space"},
-	{"tiempo": 16.114, "tecla": "space"},
-	{"tiempo": 16.314, "tecla": "space"},
-	{"tiempo": 16.514, "tecla": "space"},
-	{"tiempo": 16.722, "tecla": "space"},
-	{"tiempo": 16.962, "tecla": "space"},
-	{"tiempo": 20.114, "tecla": "space"},
-	{"tiempo": 20.33, "tecla": "space"},
-	{"tiempo": 20.546, "tecla": "space"},
-	{"tiempo": 20.754, "tecla": "space"},
-	{"tiempo": 20.986, "tecla": "space"},
-	{"tiempo": 22.026, "tecla": "space"},
-	{"tiempo": 24.211, "tecla": "space"},
-	{"tiempo": 26.139, "tecla": "space"},
-	{"tiempo": 28.139, "tecla": "space"},
-	{"tiempo": 28.355, "tecla": "space"},
-	{"tiempo": 28.571, "tecla": "space"},
-	{"tiempo": 28.795, "tecla": "space"},
-	{"tiempo": 29.011, "tecla": "space"},
-	{"tiempo": 30.083, "tecla": "space"},
+	{"tiempo": 9.921, "tecla": "space"},
+
+	{"tiempo": 11.929, "tecla": "space"},
+
+	{"tiempo": 13.898, "tecla": "space"},
+
+	{"tiempo": 15.914, "tecla": "space"},
+
+	{"tiempo": 16.138, "tecla": "space"},
+
+	{"tiempo": 16.402, "tecla": "space"},
+
+	{"tiempo": 17.506, "tecla": "space"},
+
+	{"tiempo": 18.562, "tecla": "space"},
+
+	{"tiempo": 20.01, "tecla": "space"},
+
+	{"tiempo": 21.05, "tecla": "space"},
+
+	{"tiempo": 22.043, "tecla": "space"},
+
+	{"tiempo": 23.579, "tecla": "space"},
+
+	{"tiempo": 23.843, "tecla": "space"},
+
+	{"tiempo": 24.043, "tecla": "space"},
+
+	{"tiempo": 24.235, "tecla": "space"},
+
+	{"tiempo": 24.459, "tecla": "space"},
+
+	{"tiempo": 24.675, "tecla": "space"},
+
+	{"tiempo": 24.867, "tecla": "space"},
+
+	{"tiempo": 25.091, "tecla": "space"},
+
+	{"tiempo": 25.363, "tecla": "space"},
+
+	{"tiempo": 25.619, "tecla": "space"},
+
+	{"tiempo": 27.091, "tecla": "space"},
+
+	{"tiempo": 27.275, "tecla": "space"},
 
 
 ]
